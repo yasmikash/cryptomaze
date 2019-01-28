c72bf727b5d094a2f4df595b505aad16410c150e
yasmikash <yashtricksz@gmail.com>
2019-01-28 19:03:03 +0530

    Delete exchange.py
    This feature doesn't support with Cryptomaze anymore

--- cryptomaze/exchange.py
+++
@@ -1,9 +0,0 @@
-def currency_prices():
-    import urllib.request, json
-    currencies = {} 
-    with urllib.request.urlopen("https://poloniex.com/public?command=returnTicker") as url:
-        data = json.loads(url.read().decode())
-        currencies['bch'] = data['BTC_BCHABC']['last']
-        currencies['eth'] = data['BTC_ETH']['last']
-        return currencies
-
