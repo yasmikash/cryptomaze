b3125baa29376ed7774d9af1f9b823dc5e4b494d
yasmikash <yashtricksz@gmail.com>
2019-01-30 14:28:05 +0530

    Update __init__.py

--- cryptomaze/__init__.py
+++ cryptomaze/__init__.py
@@ -7,8 +7,8 @@ from cryptomaze.settings import pri_key, pub_key
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '0343c517a2005ef9540492aee77c16da'
-app.config['RECAPTCHA_PUBLIC_KEY'] = pub_key
-app.config['RECAPTCHA_PRIVATE_KEY'] = pri_key
+app.config['RECAPTCHA_PUBLIC_KEY'] = pri_key
+app.config['RECAPTCHA_PRIVATE_KEY'] = pub_key
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cryptomaze.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
